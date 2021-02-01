71857b78e449c2b300af6e9febaca1b35e4a84f5
tartarus-git <niktedig@gmail.com>
2021-02-01 16:53:32 +0100

    Making the arduino more professional and robust and bug fixes and whatever.

--- relay/arduino_relay/arduino_relay.ino
+++ relay/arduino_relay/arduino_relay.ino
@@ -1,86 +1,114 @@
 // Pins.
 #define PHOTORESISTOR A0
+#define CLEARANCE 10
 
 // Serial.
 #define BAUD_RATE 9600
+#define BUFFER_SIZE 1024
 
 // Laser.
-#define TRANSMISSION_INTERVAL 1000 // In milliseconds. TODO: Change this to microseconds.
-#define RECEIVE_OFFSET 10 // In microseconds.     // TODO: This might not even be necessary.
-#define SYNC_POINT 100
-
-#define CLEARANCE 10
-#define BUFFER_SIZE 8     // TODO: Change this to a larger buffer size and such.
-
-#define SLEEP delay(TRANSMISSION_INTERVAL);
-
 short baseline;
+
+#define DESC_BIT_DURATION 100    // milliseconds.
+
+struct ConnectionDescriptor {
+  int16_t syncInterval;         // The amount of bytes between each synchronization plus 1.
+  uint16_t bitDuration;
+  uint8_t durationType;
+} desc;
+
+void sleep() {
+  if (desc.durationType) {
+    delayMicroseconds(desc.bitDuration);
+    return;
+  }
+  delay(desc.bitDuration);
+}
+
+int16_t syncCounter = -1;
+
+void sync() {
+  while (analogRead(PHOTORESISTOR) > baseline) { }
+  while (analogRead(PHOTORESISTOR) <= baseline) { }
+  sleep();
+}
 
 void setup() {
   // Initialize serial.
   Serial.begin(BAUD_RATE);
 
-  while (!Serial) { }         // Wait for initialization. This is because the serial-to-usb chip is async. This isn't necessary for serial pins.
-
-  // Pins are inputs by default, so no need for pinMode() here.
-
-  pinMode(13, OUTPUT); // Except this one, which we are using for debugging purposes. TODO.
+  // Wait for initialization. This is because the serial-to-usb chip is async. This isn't necessary for serial pins.
+  while (!Serial) { }
 
   // Set the baseline brightness to the environmental brightness plus the clearance.
   baseline = analogRead(PHOTORESISTOR) + CLEARANCE;
-}
 
-short syncCounter = 0;
-
-short pos = 0;
-char charPos = 0;
-char buffer[BUFFER_SIZE];
-
-void sendBuffer() {
-  // Send the buffer to computer over serial.
-  Serial.write(buffer, pos + 1);      // TODO: If this is the only thing in here, then there isn't really a need for a function is there.
+  Serial.println("Waiting to receive connection descriptor.");
   Serial.flush();
-}
 
-bool ledState = LOW;
-
-void incrementPos() {   // TODO: You have to calculate the length of the transmission somewhere, because or else you're not going to know when to send the buffer.
-  if (charPos == 7) {
-      ledState = !ledState;
-      digitalWrite(13, ledState);
-      charPos = 0;
-      if (pos == BUFFER_SIZE - 1) {
-        sendBuffer();
-        pos = 0;
-        return;
-      }
-      pos++;
-      return;
-  }
-  charPos++;
-}
-
-void loop() {
-  if (analogRead(PHOTORESISTOR) > baseline) {      // TODO: Use a non-sleep-reliant method to transfer metadata about the connection before attempting the high-speed transfer.
-    delayMicroseconds(RECEIVE_OFFSET); // TODO: This probably doesn't do anything because the arduino can't time travel. This probably makes it worse.
-    while (true) {
-      SLEEP;
-      if (syncCounter == SYNC_POINT) {
-        syncCounter = 0;
-        while (true) {
-          if (analogRead(PHOTORESISTOR) > baseline) { break; }
-        }
-        delayMicroseconds(RECEIVE_OFFSET);    // TODO: This almost definitely doesn't do anything, you can probably remove the offsets because arduino is behind.
-        continue;
-      }
-      syncCounter++;
+  // Receive the connection descriptor.
+  for (int i = 0; i < sizeof(desc); i++) {
+    while (analogRead(PHOTORESISTOR) <= baseline) { }
+    for (int j = 0; j < 8; j++) {
+      delay(DESC_BIT_DURATION);
       if (analogRead(PHOTORESISTOR) > baseline) {
-        buffer[pos] |= 1 << charPos;            // TODO: See if it's possible to use HIGH here instead of one for more intent and such.
-        incrementPos();                   // TODO: You can put this at the top of the loop so that you avoid writing it twice down here. Not without some changing around of other stuff though. See if you can make this work.
+        *((char*)&desc + i) |= HIGH << j;
         continue;
       }
-      buffer[pos] &= ~(1 << charPos);       // TODO: Maybe make the charPos shifts into some sort of function or something. Is this the most efficient way?
-      incrementPos();
+      *((char*)&desc + i) &= ~(HIGH << j);      // TODO: Is there any better way to do this?
     }
+    // Give the other device enough time to set up a synchronization point.
+    delay(DESC_BIT_DURATION);
+    delay(DESC_BIT_DURATION);
+  }
+
+  char buffer[BUFFER_SIZE];
+
+  while (true) {
+    Serial.println("Synchronizing...");
+    
+    sync();
+
+    // Receive the length of the upcoming data transmission.
+    uint16_t length;      // TODO: No sync here right? It only is possible once actually sending data right?
+    for (uint16_t i = 0; i < 16; i++) {
+      if (analogRead(PHOTORESISTOR) > baseline) {
+        length |= (uint16_t)HIGH << i;
+        sleep();
+        continue;
+      }
+      length &= ~((uint16_t)HIGH << i);
+      sleep();
+    }
+
+    Serial.println("Length received:");
+    Serial.println(length);
+
+    Serial.println("Receiving data.");
+
+    // Receive the data.
+    for (int i = 0; i < length; i++) {
+      // Synchronize if the time is right.
+      if (syncCounter == desc.syncInterval) {
+        sync();
+        syncCounter = 0;
+      } else { syncCounter++; }
+
+      // Receive the next bit.
+      for (int j = 0; j < 8; j++) {
+        if (analogRead(PHOTORESISTOR) > baseline) {
+          buffer[i] |= HIGH << j;
+          sleep();
+          continue;
+        }
+        buffer[i] &= ~(HIGH << j);
+        sleep();
+      }
+    }
+
+    // Output the buffer to serial.
+    //Serial.write(buffer, length);    TODO: Disabled for debugging purposes. Enable later.
   }
 }
+
+void loop() { }
